038f14b22536eed4e829447e3d63fc9155fa92cc
Minahil009 <minahilaziz009@gmail.com>
2021-03-29 17:42:42 +0500

    updated app

--- Assignments/KidsApp/app/src/main/java/com/example/kidsapp/MainActivity.java
+++ Assignments/KidsApp/app/src/main/java/com/example/kidsapp/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.kidsapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView =findViewById(R.id.imageView);
+        //2circles commpleted on 720
+        imageView.animate().rotation(720).setDuration(30000);
     }
+
 }
