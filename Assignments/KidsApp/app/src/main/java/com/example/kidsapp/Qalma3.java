be0330425b101e19e7a66d5a8a84337544e2a4e4
Minahil009 <minahilaziz009@gmail.com>
2021-04-02 22:29:05 +0500

    file added

---
+++ Assignments/KidsApp/app/src/main/java/com/example/kidsapp/Qalma3.java
@@ -0,0 +1,14 @@
+package com.example.kidsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Qalma3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qalma3);
+    }
+}
